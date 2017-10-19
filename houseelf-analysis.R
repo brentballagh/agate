10e4560a6819b6e634ac79dfe0f2f3faa7b8516a
Brent Ballaugh <ballagh@okstate.edu>
2017-10-19 16:11:52 -0500

    added for loop

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,6 +1,8 @@
 #this analysis is intended to drive anyone crazy mad who works on it
 
 library(stringr)
+library(dplyr)
+library(dbplyr)
 
 houself_earlength_dna_data_1 <- read.csv("./data/houseelf_earlength_dna_data.csv")
 
@@ -10,6 +12,22 @@ gc_contentelf <- function(gc_content_elf){
   gc_contentelf <- (gself + cself) / str_length(gc_content_elf) * 100
   return(gc_contentelf)
 }
+
+gc_contentelf(houself_earlength_dna_data_1$dnaseq[2])
+
+#for loop
+
+for (i in 1:length(houself_earlength_dna_data_1 $dnaseq)) {
+  houself_earlength_dna_data_1$gc_content[i] <- gc_contentelf(houself_earlength_dna_data_1$dnaseq[i])
+}
+
+
+
+
+
+
+
+
 
 
 
