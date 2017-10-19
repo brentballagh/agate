de70bc5c6ed924abe141a6d4a9e13d4e20f56d19
Brent Ballaugh <ballagh@okstate.edu>
2017-10-19 16:10:26 -0500

    added function to calculate gc content and save it to houseelf data

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,6 +1,17 @@
 #this analysis is intended to drive anyone crazy mad who works on it
 
+library(stringr)
+
 houself_earlength_dna_data_1 <- read.csv("./data/houseelf_earlength_dna_data.csv")
+
+gc_contentelf <- function(gc_content_elf){
+  gself <- str_count(gc_content_elf, "G")
+  cself <- str_count(gc_content_elf, "C")
+  gc_contentelf <- (gself + cself) / str_length(gc_content_elf) * 100
+  return(gc_contentelf)
+}
+
+
 
 
 
